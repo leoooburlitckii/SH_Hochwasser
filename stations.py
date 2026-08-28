29c37c5bd046cc3261f8fef05c0f3c85dbc24788
burlitskiy <leonid.burlitckii@gmail.com>
2026-08-28 15:18:52 +0200

    1. Map fertig 2. Jede station hat koordinaten 3. dashboard.py auf aktuelle map aktualisiert

--- stations.py
+++ stations.py
@@ -30,7 +30,7 @@ STATIONEN = [
     {
         "name": "WITTDÜN",
         "uuid" : "9c4c11f2-0548-4555-beac-ecfd36f9bd74",
-        "row" : 5,
+        "row" : 6,
         "col": 2
     },
 
@@ -38,358 +38,337 @@ STATIONEN = [
         "name": "PELLWORM ANLEGER",
         "uuid" : "2852b9ab-d30e-4d04-ae06-3e946f48a0b1",
         "row" : 8,
-        "col": 3
+        "col": 8
     },
 
     {
         "name": "HUSUM",
         "uuid" : "e114aeec-c8d9-4d20-8fe1-8822058cb38b",
-        "row" : 1,
-        "col": 1
+        "row" : 9,
+        "col": 16
     },
 
     {
         "name": "FLENSBURG",
         "uuid" : "9e19c411-f728-4a43-a057-39d4155c71cc",
-        "row" : 1,
-        "col": 1
+        "row" : 4,
+        "col": 27
     },
 
     {
         "name": "LANGBALLIGAU",
         "uuid" : "5a33bf14-6bdc-4666-b2e3-ac78e3083e2a",
-        "row" : 1,
-        "col": 1
+        "row" : 4,
+        "col": 33
     },
 
     {
         "name": "SCHLESWIG",
         "uuid" : "09370c05-1041-4395-a5d4-b8db6e59c4c8",
-        "row" : 1,
-        "col": 1
+        "row" : 8,
+        "col": 32
     },
 
     {
         "name": "KAPPELN",
         "uuid" : "b09f2243-60f0-469a-8f3b-0ea6abc83267",
-        "row" : 1,
-        "col": 1
+        "row" : 5,
+        "col": 40
     },
 
     {
         "name": "ECKERNFÖRDE",
         "uuid" : "1faa9b2c-c269-4662-af70-ef11da27cc1c",
-        "row" : 1,
-        "col": 1
+        "row" : 9,
+        "col": 36
     },
 
     {
         "name": "LT KIEL",
         "uuid" : "736437d7-0f6f-41b7-bc69-5ed721da4f85",
-        "row" : 1,
-        "col": 1
+        "row" : 8,
+        "col": 48
     },
 
     {
         "name": "MARIENLEUCHTE",
         "uuid" : "8effc15d-8583-4ac6-9f42-1b63c47f92b0",
-        "row" : 1,
-        "col": 1
+        "row" : 9,
+        "col": 68
     },
 
     {
         "name": "HEILIGENHAFEN",
         "uuid" : "06219dd9-a2c4-463c-9619-623b3c026cbc",
-        "row" : 1,
-        "col": 1
+        "row" : 12,
+        "col": 64
     },
 
     {
         "name": "NEUSTADT",
         "uuid" : "3f0b6b74-80a9-4576-a3cb-ea967dfc349f",
-        "row" : 1,
-        "col": 1
+        "row" : 15,
+        "col": 61
     },
 
     {
         "name": "BÜSSAU UP",
         "uuid" : "bf7bb8e8-e81f-4655-9e84-28738feae936",
-        "row" : 1,
-        "col": 1
+        "row" : 22,
+        "col": 54
     },
 
     {
         "name": "KIEL-HOLTENAU",
         "uuid" : "3ad4013f-644b-47f5-a641-44b332bfecb2",
-        "row" : 1,
-        "col": 1
+        "row" : 11,
+        "col": 44
     },
 
     {
         "name": "NOK KIEL AUSSEN",
         "uuid" : "6dc44585-5b88-45d7-9c64-7c845408b698",
-        "row" : 1,
-        "col": 1
+        "row" : 12,
+        "col": 44
     },
 
     {
         "name": "NOK KIEL BINNEN",
         "uuid" : "8af24d6a-eecd-434e-9f1a-60ab6bc05490",
-        "row" : 1,
-        "col": 1
+        "row" : 12,
+        "col": 43
     },
 
     {
         "name": "AWK STROHBRÜCK",
         "uuid" : "0e192297-1224-4465-b1c7-a476cc17f74d",
-        "row" : 1,
-        "col": 1
+        "row" : 11,
+        "col": 38
     },
 
     {
         "name": "NOK KÖNIGSFÖRDE",
         "uuid" : "d0ec2790-a870-405e-bb61-2dcc3cf47467",
-        "row" : 1,
-        "col": 1
+        "row" : 11,
+        "col": 36
     },
 
     {
         "name": "NOK RENDSBURG",
         "uuid" : "8c8afb56-88ea-483a-a7d4-033c22f53497",
-        "row" : 1,
-        "col": 1
+        "row" : 12,
+        "col": 33
     },
 
     {
         "name": "NOK BREIHOLZ",
         "uuid" : "4a904d59-d48f-4878-af7b-cbd955f342e0",
-        "row" : 1,
-        "col": 1
+        "row" : 14,
+        "col": 27
     },
 
     {
         "name": "LEXFÄHRE OBERWASSER",
         "uuid" : "86c5688f-2fac-4d58-a245-ad8ce14cafbf",
-        "row" : 1,
-        "col": 1
+        "row" : 13,
+        "col": 25
     },
 
     {
         "name": "LEXFÄHRE UNTERWASSER",
         "uuid" : "7f01fbd8-653c-40ba-8ed0-57386a9b4557",
-        "row" : 1,
-        "col": 1
+        "row" : 12,
+        "col": 25
     },
     
     {
         "name": "BÜSUM",
         "uuid" : "5287a3e1-c540-4ab1-b52e-880d124cbc43",
-        "row" : 1,
-        "col": 1
+        "row" : 15,
+        "col": 14
     },
 
     {
         "name": "TÖNNING",
         "uuid" : "00e386ac-e35c-4a6e-80dd-f8cd8a9e7a62",
-        "row" : 1,
-        "col": 1
+        "row" : 12,
+        "col": 17
     },
 
     {
         "name": "FRIEDRICHSTADT STRASSENBRÜCKE",
         "uuid" : "721313e7-935a-4bb8-8c6d-0e014211b2ec",
-        "row" : 1,
-        "col": 1
+        "row" : 11,
+        "col": 19
     },
 
     {
         "name": "FRIEDRICHSTADT TREENE",
         "uuid" : "795ce865-3ac4-43b8-88d5-049a34e4359e",
-        "row" : 1,
-        "col": 1
+        "row" : 10,
+        "col": 19
     },
 
     {
         "name": "NORDFELD OBERWASSER",
         "uuid" : "61394669-3f1b-44e5-ae37-50fd2512f4fa",
-        "row" : 1,
-        "col": 1
+        "row" : 12,
+        "col": 20
 
     },
 
     {
         "name": "NORDFELD UNTERWASSER",
         "uuid" : "cb93548e-c1ce-43f6-b3a1-6903f8835b33",
-        "row" : 1,
-        "col": 1
+        "row" : 11,
+        "col": 20
     },
 
     {
         "name": "EIDER-SPERRWERK BP",
         "uuid" : "8ac85e6c-6167-496c-b16f-fa94f81cd94a",
-        "row" : 1,
-        "col": 1
+        "row" : 13,
+        "col": 16
     },
 
     {
         "name": "EIDER-SPERRWERK AP",
         "uuid" : "04acd7e5-3cbc-4cdd-b4a9-f452e868f4d6",
-        "row" : 1,
-        "col": 1
+        "row" : 13,
+        "col": 15
     },
 
     {
         "name": "NOK DÜKERSWISCH",
         "uuid" : "3954300d-f112-4fe6-8d0a-b06496372e36",
-        "row" : 1,
-        "col": 1
+        "row" : 17,
+        "col": 24
     },
 
     {
         "name": "NOK BRUNSBÜTTEL",
         "uuid" : "85fc0dac-a53f-4638-880d-40b24bb282f3",
-        "row" : 1,
-        "col": 1
+        "row" : 18,
+        "col": 20
     },
 
     {
         "name": "BRUNSBÜTTEL MPM",
         "uuid" : "d4f5f719-8c52-4f8d-945d-1c31404cc628",
-        "row" : 1,
-        "col": 1
+        "row" : 19,
+        "col": 20
     },
 
     {
         "name": "GRÖNHUDE",
         "uuid" : "15859426-834c-429e-9c41-2e097b717b1d",
-        "row" : 1,
-        "col": 1
+        "row" : 18,
+        "col": 33
     },
 
     {
         "name": "BREITENBERG",
         "uuid" : "24c6a014-864b-4d53-bd05-0b49106f5412",
-        "row" : 1,
-        "col": 1
+        "row" : 18,
+        "col": 32
     },
 
     {
         "name": "ITZEHOE HAFEN",
         "uuid" : "d863cbc3-5e5e-4095-855c-026f0850dd58",
-        "row" : 1,
-        "col": 1
+        "row" : 18,
+        "col": 31
     },
 
     {
         "name": "BROKDORF",
         "uuid" : "610ab204-d3c4-4a11-a38b-e31461fdcf27",
-        "row" : 1,
-        "col": 1
+        "row" : 19,
+        "col": 24
     },
 
     {
         "name": "STÖR-SPERRWERK AP",
         "uuid" : "d9acdbec-61ff-4308-978a-2f4d1c2c4059",
-        "row" : 1,
-        "col": 1
+        "row" : 20,
+        "col": 25
     },
 
     {
         "name": "GLÜCKSTADT",
         "uuid" : "1f1bbed7-c1fa-45b4-90d3-df94b50ad631",
-        "row" : 1,
-        "col": 1
+        "row" : 21,
+        "col": 26
     },
 
     {
         "name": "KOLLMAR",
         "uuid" : "3ed90357-4b01-4119-b1c5-bd2c62871e7b",
-        "row" : 1,
-        "col": 1
+        "row" : 22,
+        "col": 28
     },
 
     {
         "name": "KRÜCKAU-SPERRWERK AP",
         "uuid" : "a653eb04-de2e-47f7-8e2c-09277cfe95ae",
-        "row" : 1,
-        "col": 1
+        "row" : 22,
+        "col": 29
     },
 
     {
         "name": "ELMSHORN HAFEN",
         "uuid" : "136febf6-1371-4118-a9b8-4275444bda5d",
-        "row" : 1,
-        "col": 1
+        "row" : 22,
+        "col": 31
     },
 
     {
         "name": "PINNAU-SPERRWERK AP",
         "uuid" : "391bbba5-83d4-4791-b07e-65ab29b423f6",
-        "row" : 1,
-        "col": 1
+        "row" : 23,
+        "col": 30
     },
 
     {
         "name": "UETERSEN",
         "uuid" : "575da86f-d975-4837-b6f5-6f19c3a5e4b6",
-        "row" : 1,
-        "col": 1
+        "row" : 23,
+        "col": 32
     },
 
     {
         "name": "HETLINGEN",
         "uuid" : "599c23b1-4550-41e3-a7e7-3056989927f1",
-        "row" : 1,
-        "col": 1
+        "row" : 24,
+        "col": 31
     },
 
     {
         "name": "SCHULAU",
         "uuid" : "f3c6ee73-5561-4068-96ec-364016e7d9ef",
-        "row" : 1,
-        "col": 1
-    },
-
-    {
-        "name": "HELGOLAND SÜDHAFEN",
-        "uuid" : "0d8233b8-36c9-4a24-ad77-a4e7c617932f",
-        "row" : 1,
-        "col": 1
-    },
-
-    {
-        "name": "HELGOLAND BINNENHAFEN",
-        "uuid" : "c0ec139b-13b4-4f86-bee3-06665ad81a40",
-        "row" : 1,
-        "col": 1
-    },
-
-    {
-        "name": "HOHNSTORF",
-        "uuid" : "d9289367-c8aa-4b6a-b1ad-857fec94c6bb",
-        "row" : 1,
-        "col": 1
+        "row" : 24,
+        "col": 35
     },
 
     {
         "name": "GEESTHACHT",
         "uuid" : "44f7e955-c97d-45c8-9ed7-19406806fb4c",
-        "row" : 1,
-        "col": 1
+        "row" : 27,
+        "col": 49
     },
 
     {
         "name": "DONNERSCHLEUSE OP",
         "uuid" : "45634232-36ac-416c-806d-5f64201dae2c",
-        "row" : 1,
-        "col": 1
+        "row" : 24,
+        "col": 54
     },
 
     {
         "name": "MÖLLN",
         "uuid" : "46644438-83d6-4da8-a2a1-1deb2cb67561",
-        "row" : 1,
-        "col": 1
+        "row" : 25,
+        "col": 54
     }
    
 ]
