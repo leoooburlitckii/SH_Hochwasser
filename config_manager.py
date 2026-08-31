9d5dbcf91c45c6042838852eccb380c190d3f548
burlitskiy <leonid.burlitckii@gmail.com>
2026-08-31 15:51:41 +0200

    config_manager fertgi welche erlaubt stationen in user_config.json hinzufügen um die dann personalisierte Karte auszugeben

---
+++ config_manager.py
@@ -0,0 +1,63 @@
+import json
+from stations import STATIONEN
+
+CONFIG_FILE = "user_config.json"
+
+def configure_stations():
+    print("=" * 60)
+
+
+    for idx, station in enumerate(STATIONEN, start=1):
+        print(f"[{idx:>2}] {station['name']:<30}", end="\n" if idx % 2 == 0 else "  ")
+    
+    print("-" * 60)
+    print("Wähle Stationen nacheinander aus. (Leere Eingabe zum Beenden)")
+    
+    selected_list = []
+    used_symbols = set()
+
+    #Station wählen
+    while True:
+        choice = input("\n Stationsnummer eingeben (Enter=Fertig): ").strip()
+        if not choice:
+            break
+
+        if not choice.isdigit() or not (1 <= int(choice) <= len(STATIONEN)):
+            print("Ungültige Eingabe, wiederholen Sie")
+            continue
+
+        idx = int(choice) -1
+        st = STATIONEN[idx]
+
+        #Symbol wählen
+        while True:
+            symbol = input(f"1 Zeichen Symbol für {st['name']} eingeben: ").strip()
+            if len(symbol) != 1:
+                print("Ungültige Eingabe, Wiederholen Sie")
+            elif symbol in used_symbols:
+                print(f"Symbol {symbol} wird schon für eine andere Station verwendet")
+            else:
+                break
+
+        used_symbols.add(symbol)
+
+        selected_list.append({
+            "symbol" :symbol,
+            "name": st["name"],
+            "uuid": st["uuid"],
+            "row": st["row"],
+            "col": st["col"]
+            
+        })
+
+        print(f"Station {st['name']} mit Symbol {symbol} hinzugefügt")
+
+    if selected_list:
+        with open(CONFIG_FILE, "w", encoding="utf-8") as f:
+            json.dump(selected_list, f, indent=4, ensure_ascii=False)
+        print(f"\n{len(selected_list)} Stationen erfolgreich in '{CONFIG_FILE}' gespeichert!")
+    else:
+        print("\nKeine Änderungen gespeichert.")
+
+if __name__  == "__main__":
+    configure_stations()
